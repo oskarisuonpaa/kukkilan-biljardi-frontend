b8edeb8064121d60738cd0ef813b5e2078ebee77
Oskari Suonpää <oskarisuonpaa@proton.me>
2025-09-08 08:44:06 +0300

    Implement code changes to enhance functionality and improve performance

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,7 @@
+import SectionWrapper from "@/components/SectionWrapper";
 import { fetchContactInfo } from "../lib/api";
 import { ContactInfoItem } from "../lib/definitions";
+import Image from "next/image";
 
 // TODO: About us data from backend
 
@@ -13,42 +15,41 @@ Tervetuloa!`,
 
   return (
     <main>
-      {/* About Us */}
       <section>
         <header>
-          <h2>About Us</h2>
-          <div className="section-undeline" />
+          <Image
+            src="/kuksnookbiledit.png"
+            alt="Kukkilan Biljardi"
+            width={350}
+            height={0}
+            className="h-auto w-auto justify-self-center"
+            priority
+          />
+          <div className="section-underline mb-6" aria-hidden="true" />
         </header>
-        <div className="space-y-4 text-[var(--text-secondary)] leading-relaxed">
+        <div className="space-y-4 leading-relaxed text-center">
           {mockAboutUsData.map((data, id) => (
             <p key={id}>{data}</p>
           ))}
         </div>
       </section>
 
-      {/* Contact Us */}
-      <section>
-        <header>
-          <h2>Contact Us</h2>
-          <div className="section-undeline" />
-        </header>
-        <ul className="space-y-3 text-[var(--text-secondary)]">
-          <li>
-            <span className="font-medium text-[var(--text-main)]">
-              Address:
-            </span>{" "}
-            {contactInfo.address}
-          </li>
-          <li>
-            <span className="font-medium text-[var(--text-main)]">Phone:</span>{" "}
-            {contactInfo.phone}
-          </li>
-          <li>
-            <span className="font-medium text-[var(--text-main)]">Email:</span>{" "}
-            {contactInfo.email}
-          </li>
-        </ul>
-      </section>
+      <SectionWrapper title="Yhteystiedot">
+        <div className="space-y-4 leading-relaxed">
+          <ul className="space-y-3">
+            <li>
+              <span className="font-medium">Osoite:</span> {contactInfo.address}
+            </li>
+            <li>
+              <span className="font-medium">Puhelin:</span> {contactInfo.phone}
+            </li>
+            <li>
+              <span className="font-medium">Sähköposti:</span>{" "}
+              {contactInfo.email}
+            </li>
+          </ul>
+        </div>
+      </SectionWrapper>
     </main>
   );
 };
